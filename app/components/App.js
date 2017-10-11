c381df3cdf6f073799ea41ea6d11f2a70139ef2f
Taylor <kenostik@gmail.com>
2017-10-10 23:17:53 -0700

    React Project Setup

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+	render () {
+		console.log("Hello World!");
+
+		return (
+			<h1>Hello World!</h1>
+		);
+	}
+}
